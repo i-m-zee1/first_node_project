726febc84a5929e734cff0590087241fbb1029c7
i-m-zee1 <imzee.au@gmail.com>
2024-10-16 19:23:14 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# first_node_project
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/i-m-zee1/first_node_project)
---
+++ index.js
@@ -0,0 +1,6 @@
+// run `node index.js` in the terminal
+
+// console.log(`Hello Node.js v${process.versions.node}!`);
+const mission = 'learn';
+if (mission === 'learn') console.log('Time to write some node code!!!!!!!');
+else console.log(`Is ${mission} really more fun?????`);
