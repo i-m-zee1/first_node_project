4847f1d18c9d1c63954ca13375c278a083eb7249
i-m-zee1 <imzee.au@gmail.com>
2024-10-28 23:42:43 +0500

    Proces use in node

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // run `node index.js` in the terminal
 
 // console.log(`Hello Node.js v${process.versions.node}!`);
-const mission = 'learn';
+const mission = process.argv[2]//'learn';
+console.log("Process: ",mission);
 if (mission === 'learn') console.log('Time to write some node code!!!!!!!');
 else console.log(`Is ${mission} really more fun?????`);
